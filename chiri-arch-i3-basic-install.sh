894128f1befe85af3b591a10aa2519fdddd07faa
Cristian Orellana <cristian.orellana.m@gmail.com>
2022-08-07 10:55:29 -0400

    Adds a more general case for partitioning the disks.
    The idea of it being fully automated for different existing
    conditions.  Tested.

--- chiri-arch-i3-basic-install.sh
+++ chiri-arch-i3-basic-install.sh
@@ -10,6 +10,16 @@
 
 ## CONFIGURATION OPTIONS
 KEYBOARD='la-latin1';
+
+# Partition configuration.  Follows the sfdisk script file format.
+BOOT_PARTITION_FILE="./boot-partition";
+SWAP_PARTITION_FILE="./swap-partition";
+LINUX_PARTITION_FILE="./linux-partition";
+echo "size=300MiB, bootable, type=uefi" > $BOOT_PARTITION_FILE;
+echo "size=512MiB, type=swap" > $SWAP_PARTITION_FILE;
+echo "type=linux" > $LINUX_PARTITION_FILE;
+
+# Packages.
 PACSTRAP_LIST_FILE='pacstrap-packages';
 cat <<EOT >> $PACSTRAP_LIST_FILE
 base
@@ -61,6 +71,7 @@ grub
 efibootmgr
 EOT
 
+
 ## KEYBOARD LAYOUT
 ## For now just latin-american keyboard.
 loadkeys $KEYBOARD;
@@ -75,23 +86,152 @@ loadkeys $KEYBOARD;
 timedatectl set-ntp true;
 
 
-## PARTITION DISK AND MAKE AND MOUNT FILESYSTEMS.
-## This is for a 10GB disk, using GPT and UEFI, 3 partitions:
-## boot, swap and root.
-parted /dev/sda mklabel gpt;
-parted /dev/sda mkpart boot fat32 1049KB 316MB set 1 esp on;
-parted /dev/sda mkpart swap linux-swap 316MB 852MB set 2 swap on;
-parted /dev/sda mkpart root ext4 852MB 100%;
+## PARTITION DISK, MAKE AND MOUNT FILESYSTEMS.
+# This piece of code checks whether there are existing partitions,
+# their type, label type, etc.  And creates the new ones based on
+# missing ones.  It does not currently alter the existing ones but
+# leaves them as is.
+if [[ -a /proc/partitions ]]; then
+    # See whether machine was booted in UEFI mode.
+    UEFI_BOOT=0;
+    if [[ -a /sys/firmware/efi/fw_platform_size ]]; then UEFI_BOOT=1; fi;
+    
+    # Detect first disk's name.
+    echo "Detecting first disk device..."
+    DISK=`sed -n '3l' /proc/partitions | tr -s " " | cut -d " " -f 5 | cut -c 1-3`;
+    if [[ -z $DISK ]]; then
+        echo "No devices or partitions listed in /proc/partitions. Exiting.";
+        exit 1;
+    else
+        echo "First disk device is: $DISK";
 
-# Make the filesystems.
-mkfs.fat -F 32 /dev/sda1;
-mkswap /dev/sda2;
-mkfs.ext4 /dev/sda3;
+        # Check if disk has label type and detect.
+        echo "Inspecting existing partitions and disk label..."
+        HAS_LABEL=`fdisk -l /dev/$DISK | grep -E 'gpt|dos'`;
+        if [[ ! -z $HAS_LABEL ]]; then
+            IS_GPT=`echo $HAS_LABEL | grep -c 'gpt'`;
 
-# Mount the filesystems.
-mount --mkdir /dev/sda3 /mnt;
-mount --mkdir /dev/sda1 /mnt/boot;
-swapon /dev/sda2;
+            # Check partitions.
+            if [[ $IS_GPT -gt 0 ]]; then
+                echo "Disk label type is: GPT";
+
+                BOOT_PARTITION=`fdisk -l /dev/$DISK | grep -E "$DISK[0-9].*EFI" | cut -d " " -f 1`;
+                if [[ -z $BOOT_PARTITION ]]; then
+                    echo "No EFI partition detected, creating...";
+                    cat $BOOT_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+                    BOOT_PARTITION_STRLEN=`echo $BOOT_PARTITION | wc -m`;
+                    BOOT_PARTITION_NUM=`echo $BOOT_PARTITION | cut -c $(( $BOOT_PARTITION_STRLEN - 1 ))`;
+                    set $BOOT_PARTITION_NUM esp on;
+                    echo "Making filesystem for EFI partition...";
+                    mkfs.fat -F 32 /dev/$DISK$BOOT_PARTITION_NUM;
+                    echo "DONE";
+                else
+                    echo "Existing EFI partition detected.";
+                    BOOT_PARTITION_STRLEN=`echo $BOOT_PARTITION | wc -m`;
+                    BOOT_PARTITION_NUM=`echo $BOOT_PARTITION | cut -c $(( $BOOT_PARTITION_STRLEN - 1 ))`;
+                fi;
+            else
+                echo "Disk label type is: DOS";
+            fi;
+
+            SWAP_PARTITION=`fdisk -l /dev/$DISK | grep -E "$DISK[0-9].*swap" | cut -d " " -f 1`;
+            if [[ -z $SWAP_PARTITION ]]; then
+                echo "No SWAP partition detected, creating...";
+                cat $SWAP_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+                SWAP_PARTITION_STRLEN=`echo $SWAP_PARTITION | wc -m`;
+                SWAP_PARTITION_NUM=`echo $SWAP_PARTITION | cut -c $(( $SWAP_PARTITION_STRLEN - 1 ))`;
+                echo "Making filesystem for SWAP partition...";
+                mkswap /dev/$DISK$SWAP_PARTITION_NUM;
+                echo "DONE";
+            else
+                echo "Existing SWAP partition detected.";
+                SWAP_PARTITION_STRLEN=`echo $SWAP_PARTITION | wc -m`;
+                SWAP_PARTITION_NUM=`echo $SWAP_PARTITION | cut -c $(( $SWAP_PARTITION_STRLEN - 1 ))`;
+            fi;
+
+            LINUX_PARTITION=`fdisk -l /dev/$DISK | grep -E "$DISK[0-9].*Linux" | grep -v 'swap' | cut -d " " -f 1 | head -n 1`;
+            if [[ -z $LINUX_PARTITION ]]; then
+                echo "No LINUX partition detected, creating...";
+                cat $LINUX_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+                LINUX_PARTITION_STRLEN=`echo $LINUX_PARTITION | wc -m`;
+                LINUX_PARTITION_NUM=`echo $LINUX_PARTITION | cut -c $(( $LINUX_PARTITION_STRLEN - 1 ))`;
+                echo "Making filesystem for LINUX partition...";
+                mkfs.ext4 /dev/$DISK$LINUX_PARTITION_NUM;
+                echo "DONE";
+            else
+                echo "Existing LINUX partition detected.";
+                LINUX_PARTITION_STRLEN=`echo $LINUX_PARTITION | wc -m`;
+                LINUX_PARTITION_NUM=`echo $LINUX_PARTITION | cut -c $(( $LINUX_PARTITION_STRLEN - 1 ))`;
+            fi;
+
+            # Mount the filesystems.
+            echo "Mounting filesystems...";
+            if ! cat /proc/mounts | grep -q "/dev/$DISK$LINUX_PARTITION_NUM"; then
+                mount --mkdir /dev/$DISK$LINUX_PARTITION_NUM /mnt;
+            fi;
+            if ! cat /proc/mounts | grep -q "/dev/$DISK$BOOT_PARTITION_NUM"; then
+                mount --mkdir /dev/$DISK$BOOT_PARTITION_NUM /mnt/boot;
+            fi;
+            if ! swapon -s | grep -q "/dev/$DISK$SWAP_PARTITION_NUM"; then
+                swapon /dev/$DISK$SWAP_PARTITION_NUM;
+            fi;
+            echo "DONE";
+        else
+            
+            # No prior label or partitions.
+            echo "No partitions or type label detected, creating...";
+            if [[ UEFI_BOOT -gt 0 ]]; then
+                parted /dev/sda mklabel gpt;
+                cat $BOOT_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+                cat $SWAP_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+                cat $LINUX_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+
+                # Make the filesystems.
+                echo "Making filesystem for partitions...";
+                mkfs.fat -F 32 /dev/$DISK\1;
+                mkswap /dev/$DISK\2;
+                mkfs.ext4 /dev/$DISK\3;
+                echo "DONE";
+
+                # Mount the filesystems.
+                echo "Mounting filesystems...";
+                if ! cat /proc/mounts | grep -q "/dev/$DISK[3]"; then
+                    mount --mkdir /dev/$DISK\3 /mnt;
+                fi;
+                if ! cat /proc/mounts | grep -q "/dev/$DISK[1]"; then
+                    mount --mkdir /dev/$DISK\1 /mnt/boot;
+                fi;
+                if ! swapon -s | grep -q "/dev/$DISK\2"; then
+                    swapon /dev/$DISK\2;
+                fi;
+                echo "DONE";
+            else
+                parted /dev/sda mklabel msdos;
+                cat $SWAP_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+                cat $LINUX_PARTITION_FILE | sfdisk --append --lock /dev/$DISK;
+
+                # Make the filesystems.
+                echo "Making filesystem for partitions...";
+                mkswap /dev/$DISK\1;
+                mkfs.ext4 /dev/$DISK\2;
+                echo "DONE";
+
+                # Mount the filesystems.
+                echo "Mounting filesystems...";
+                if ! cat /proc/mounts | grep -q "/dev/$DISK[2]"; then
+                    mount --mkdir /dev/$DISK\2 /mnt;
+                fi;
+                if ! swapon -s | grep -q "/dev/$DISK[1]"; then
+                    swapon /dev/$DISK\1;
+                fi;
+                echo "DONE";
+            fi;
+        fi;
+    fi;
+else
+    echo "/proc/partitions not found. Exiting.";
+    exit 1;
+fi;
 
 
 ## SELECT MIRRORS AND INSTALL BASE SYSTEM.
